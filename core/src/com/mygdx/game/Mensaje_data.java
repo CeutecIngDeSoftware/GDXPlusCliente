3ad06b2ea3eb3410e826806294c620479234bafa
Gadi Orellana <gadijaniel@unite.edu>
2015-05-25 07:58:03 -0600

    Prueba

---
+++ core/src/com/mygdx/game/Mensaje_data.java
@@ -0,0 +1,16 @@
+package com.mygdx.game;
+
+/**
+ * Created by DELL-INSPIRON15R on 09/05/2015.
+ */
+
+import java.io.Serializable;
+
+public class Mensaje_data implements Serializable {
+    private static final long serialVersionUID = 9178463713495654837L;
+
+    public int Action;
+    public String texto;
+    public boolean last_msg=false;
+
+}
